a6c69a100648353a9109b424c946dd8c9b561d44
Rafael-Mota-Moraes <rafaelmota622@gmail.com>
2022-12-30 10:18:32 -0300

    Fixtures de receita

--- recipes/views.py
+++ recipes/views.py
@@ -15,15 +15,15 @@ def home(request):
 
 
 def category(request, category_id):
-    recipes = Recipe.objects.filter(
+    recipes = get_list_or_404(Recipe.objects.filter(
         is_published=True,
         category__id=category_id
-    ).order_by('-id')
+    ).order_by('-id'))
 
 
     return render(request, 'recipes/pages/category.html', context={
         'recipes': recipes,
-        'title': f'{recipes[0].category.name} - Category |', # type: ignore
+        'title': 'Category |', # type: ignore
     })
 
 
